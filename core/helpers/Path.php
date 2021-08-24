a408fb5d949426a9895de34d108ee9b5965aa762
Jakub Zolcik <j.zolcik@allblue.pl>
2021-08-24 10:19:07 +0200

    Development.

--- core/helpers/Path.php
+++ core/helpers/Path.php
@@ -5,6 +5,31 @@ defined('_ESPADA') or die(NO_ACCESS);
 class Path
 {
 
+    static public function Data($package_name, $file_path)
+	{
+        if (!file_exists(PATH_DATA))
+            mkdir(PATH_DATA);
+
+		$package_name = mb_strtolower($package_name);
+		$package_path = PATH_DATA . '/' . $package_name;
+		if (!file_exists($package_path))
+	  		mkdir($package_path, 0777, true);
+
+		$package_name = mb_strtolower($package_name);
+		    $fs_file_path = PATH_DATA . '/' . $package_name . '/' . $file_path;
+		    $fs_file_path = $package_path . '/' . $file_path;
+
+		return $fs_file_path;
+	}
+
+    static public function Data_Exists($package_name, $file_path)
+	{
+		$package_name = mb_strtolower($package_name);
+        $fs_file_path = PATH_DATA . '/' . $package_name . '/' . $file_path;
+
+		return file_exists($fs_file_path);
+	}
+
 	static public function File($e_path)
 	{
 		return File::Path($e_path);
@@ -16,13 +41,13 @@ class Path
             mkdir(PATH_MEDIA);
 
 		$package_name = mb_strtolower($package_name);
-		$package_media_path = PATH_MEDIA . '/' . $package_name;
-		if (!file_exists($package_media_path))
-	  		mkdir($package_media_path, 0777, true);
+		$package_path = PATH_MEDIA . '/' . $package_name;
+		if (!file_exists($package_path))
+	  		mkdir($package_path, 0777, true);
 
 		$package_name = mb_strtolower($package_name);
 		    $fs_file_path = PATH_MEDIA . '/' . $package_name . '/' . $file_path;
-		    $fs_file_path = $package_media_path . '/' . $file_path;
+		    $fs_file_path = $package_path . '/' . $file_path;
 
 		return $fs_file_path;
 	}
